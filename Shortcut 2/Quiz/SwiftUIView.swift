3c8a50c581b359b83ab460d480998abda327630e
Pavlo Bilashchuk <paulminami@live.com>
2024-02-27 17:15:20 -0500

    Add Quiz File

---
+++ Shortcut 2/Quiz/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Shortcut-2
+//
+//  Created by Pavlo Bilashchuk on 2/27/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Quiz File")
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
